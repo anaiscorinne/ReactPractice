15cd8be6b155bd166bafc27d8a0c700439d3e943
Anais Engelmajer <anaiscorinne@gmail.com>
2016-03-09 09:40:07 -0500

    Using props, parents can send info to child

--- app/assets/javascripts/components/hello-world.js.jsx
+++ app/assets/javascripts/components/hello-world.js.jsx
@@ -2,7 +2,7 @@ class HelloWorld extends React.Component {
 	render(){
 		return(
 			<div>
-				<h1>Hello World!</h1>
+				<h1>Hello {this.props.name}!</h1>
 				{ "H" + "E" + "Y" }<br />
 				<img src="something" />
 			</div>
